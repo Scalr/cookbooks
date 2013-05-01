b10b6f17e8f62439b75325d362818e57e237d5f8
Nick Demyanchuk <spike@scalr.com>
2013-05-02 00:47:34 +0300

    easy-install -> easy_install

--- cookbooks/scalarizr/recipes/default.rb
+++ cookbooks/scalarizr/recipes/default.rb
@@ -52,7 +52,7 @@ if node[:scalarizr][:platform] == 'gce'
   		package "python-devel"
   		package "openssl-devel"
   		package "python-setuptools"
-  		execute "/usr/bin/easy-install --upgrade pyopenssl"
+  		execute "/usr/bin/easy_install --upgrade pyopenssl"
   	end
 
 end
