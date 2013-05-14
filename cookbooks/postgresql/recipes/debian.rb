40315ce435aa58058d51c0cd94bad13fd53af707
Nick Demyanchuk <spike@scalr.com>
2013-05-14 18:22:20 +0300

    postgres cookbook fixed (debian 6 support)

--- cookbooks/postgresql/recipes/debian.rb
+++ cookbooks/postgresql/recipes/debian.rb
@@ -7,8 +7,14 @@ if node[:platform] == "ubuntu" and node[:lsb][:release].to_f < 11.10
 	execute "apt-get update"
 end
 
-package "postgresql-9.1"
-package "postgresql-client-9.1"
+if node[:platform] == "debian" and node[:platform_version].to_f < 7.0
+	cookbook_file "/etc/apt/sources.list.d/squeeze-backports.list"
+	execute "apt-get update" 
+	execute "apt-get install -y -t squeeze-backports postgresql-9.1"
+else
+	package "postgresql-9.1"
+	package "postgresql-client-9.1"
+end
 
 
 service "postgresql" do
