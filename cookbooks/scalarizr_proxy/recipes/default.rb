6e0e4aecb687eddf70d3dc87591dbc0d9d5211f4
Marat Komarov <marat@scalr.com>
2013-05-30 14:02:17 +0300

    Fixed nginx cookbook

--- cookbooks/scalarizr_proxy/recipes/default.rb
+++ cookbooks/scalarizr_proxy/recipes/default.rb
@@ -11,11 +11,9 @@ package "nginx" do
   action :install
 end
 
-nginx_conf_dir = "/etc/nginx/conf.d"
-
 # Servers setup
-template "#{nginx_conf_dir}/scalarizr_gate.conf" do
-  source "scalarizr_gate.conf.erb"
+template "/etc/nginx/nginx.conf" do
+  source "nginx.conf.erb"
   mode 0644
   owner "root"
   group "root"
@@ -23,18 +21,6 @@ template "#{nginx_conf_dir}/scalarizr_gate.conf" do
     :scalr_whitelist => node['scalarizr_proxy']['whitelist']
   })
 end
-
-template "#{nginx_conf_dir}/scalr_gate.conf" do
-  source "scalr_gate.conf.erb"
-  mode 0644
-  owner "root"
-  group "root"
-  variables({
-    :scalr_ports => ["80"],
-    :scalr_addr => node['scalarizr_proxy']['scalr_addr']
-  })
-end
-
 
 service 'nginx' do
   supports :status => true, :restart => true, :reload => true
