dcdff418fff6422ad4a5becbd49fa604cb0ba73c
Marat Komarov <marat@scalr.com>
2013-10-23 11:34:51 +0300

    Added pxc dummy cookbook

---
+++ cookbooks/pxc/metadata.rb
@@ -0,0 +1,8 @@
+name             'pxc'
+maintainer       'Marat Komarov'
+maintainer_email 'marat@scalr.com'
+license          'All rights reserved'
+description      'Installs/Configures Percona Xtradb Cluster 5.5'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
