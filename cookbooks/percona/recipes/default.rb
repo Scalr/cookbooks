51d80663a8f02292c8da6bff4b50858204f2c831
Marat Komarov <marat@scalr.com>
2013-11-19 16:28:26 +0200

    install percona gpg keys with one resource

--- cookbooks/percona/recipes/default.rb
+++ cookbooks/percona/recipes/default.rb
@@ -19,12 +19,7 @@ when "ubuntu","debian","gcel"
 	end
 
 	execute "request percona key" do
-	  command "gpg --keyserver subkeys.pgp.net --recv-keys 1C4CBDCDCD2EFD2A"
-	  not_if "gpg --list-keys CD2EFD2A"
-	end
-
-	execute "install percona key" do
-	  command "gpg -a --export CD2EFD2A | apt-key add -"
+	  command "apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A"
 	  not_if "apt-key list | grep CD2EFD2A"
 	end
 
