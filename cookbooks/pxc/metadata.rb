5f250edbd9af3d1989f667d0bc3ed819846f4960
Nick Demyanchuk <spike@scalr.com>
2014-01-14 16:42:23 +0200

    Pxc recipe moved to percona cookbook

--- cookbooks/pxc/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'pxc'
-maintainer       'Marat Komarov'
-maintainer_email 'marat@scalr.com'
-license          'All rights reserved'
-description      'Installs/Configures Percona Xtradb Cluster 5.5'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends 		 "percona"
