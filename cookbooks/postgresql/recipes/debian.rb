c198405ae2ce1586d36ac8d10b3377c0c902e72c
Dmytro Korsakov <dmitry@scalr.com>
2014-03-27 15:25:24 +0200

    PostgreSQL recipes bumped to version 9.3.

--- cookbooks/postgresql/recipes/debian.rb
+++ cookbooks/postgresql/recipes/debian.rb
@@ -3,12 +3,8 @@ execute "apt-get update" do
 end
 
 if node[:platform] == "debian" or (node[:platform] == "ubuntu" and [14.04, 13.10, 12.04, 10.04].include?(node[:lsb][:release].to_f))
-        version = 9.2
-        if node[:lsb][:codename] == "saucy"
-            codename = "trusty"
-        else
-            codename = node[:lsb][:codename]
-        end
+        version = 9.3
+        codename = node[:lsb][:codename]
         execute "wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -"
         template "/etc/apt/sources.list.d/pgdg.list" do
                 source "pgdg.list.erb"
@@ -16,8 +12,6 @@ if node[:platform] == "debian" or (node[:platform] == "ubuntu" and [14.04, 13.10
                 variables( :codename => codename)
                 notifies :run, resources("execute[apt-get update]"), :immediately
         end
-elsif node[:platform] == "ubuntu" and node[:lsb][:release].to_f > 12.04
-        version = 9.2
 else
         version = 9.1
 end
--- cookbooks/postgresql/recipes/redhat.rb
+++ cookbooks/postgresql/recipes/redhat.rb
@@ -4,51 +4,20 @@ package_release = ["redhat", "oracle", "amazon"].include?(node[:platform]) ? 7 :
 package_os = node[:platform] == "centos" ? "centos" : "redhat"
     
 remote_file "/tmp/pgdg-9.rpm" do
-	source "http://yum.pgrpms.org/9.2/redhat/rhel-#{platform_version}-#{arch}/pgdg-#{package_os}92-9.2-#{package_release}.noarch.rpm"
+  source "http://yum.postgresql.org/9.3/redhat/rhel-#{platform_version}-#{arch}/pgdg-#{package_os}93-9.3-1.noarch.rpm"
 end
 
 rpm_package "/tmp/pgdg-9.rpm"
 
-=begin
-ruby_block "Exclude postgres from base" do
-  block do
-    case node.platform
-    when "redhat"
-     repocfg_paths = ["/etc/yum/pluginconf.d/rhnplugin.conf"]
-     sections = ["main"]
-    when "centos"
-     repocfg_paths = ["/etc/yum.repos.d/CentOS-Base.repo"]
-     sections = ["base", "updates"]
-    when "fedora"
-      repocfg_paths = ["/etc/yum.repos.d/fedora.repo", "/etc/yum.repos.d/fedora-updates.repo"]
-      sections = ["fedora"]
-    end
-    
-    regexp = Regexp.new("(\\[(" + sections.join("|") + ")\\][^\\[]*)")
-    
-    for cfg in repocfg_paths
-      if not FileTest.exists?(cfg)
-	next
-      end
-      repo =  File.open(cfg, 'r').read
-      repo.gsub!(regexp, "\\1exclude=postgresql*\n")
-      File.open(cfg, 'w') {|f| f.write(repo)}
-    end
-  end
-  action :create
-end
-=end
+package "postgresql93"
+package "postgresql93-server"
+package "postgresql93-devel"
 
 
-package "postgresql92"
-package "postgresql92-server"
-package "postgresql92-devel"
+execute "service postgresql-9.3 initdb"
 
-
-execute "service postgresql-9.2 initdb"
-
-service "postgresql-9.2" do
+service "postgresql-9.3" do
   action [:disable, :stop]
 end
 
-execute "sed -i \"s/.*listen_addresses.*/listen_addresses = '*'/g\" /var/lib/pgsql/9.2/data/postgresql.conf"
+execute "sed -i \"s/.*listen_addresses.*/listen_addresses = '*'/g\" /var/lib/pgsql/9.3/data/postgresql.conf"
