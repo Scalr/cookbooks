2dd1bbd78f1c5d5e3e22c52f6fe2d3e97c5f7ff5
Eugeny Kurkovich <stunko@scalr.com>
2014-05-05 13:42:07 +0400

    SCALARIZR-1409 Added mongodb with ssl support build recipe

---
+++ cookbooks/mongodb/attributes/default.rb
@@ -0,0 +1,5 @@
+# MongoDB repo settings
+
+default[:repository][:dir] = "/tmp/chef-solo/mongodb-src"
+default[:repository][:name] = "git://github.com/mongodb/mongo.git"
+default[:repository][:revision] = "r2.6.0"
---
+++ cookbooks/mongodb/recipes/mod_ssl.rb
@@ -0,0 +1,40 @@
+case node[:platform_family]
+# Debian like os
+when "debian"
+    # Install requirements
+	execute "sudo apt-get update && sudo apt-get install git-core build-essential scons libssl-dev -y"
+
+# Red hat like os
+when "rhel"
+    # Install requirements
+	execute "sudo yum install git scons gcc-c++ glibc-devel openssl-devel -y"
+end
+
+# Get source
+git node[:repository][:dir] do
+    repository node[:repository][:name]
+    checkout_branch node[:repository][:revision]
+    action :checkout
+end
+
+# Build from source
+bash "install_mongodb" do
+    cwd node[:repository][:dir]
+    timeout 7200
+    returns 0
+    code <<-EOH
+        sudo scons  --cc=gcc --ssl all
+        sudo scons install
+    EOH
+end
+
+# Change service state
+service "mongodb" do
+    action [ :disable, :stop ]
+end
+
+execute "sudo rm -Rf #{node[:repository][:dir]}"
+
+
+
+
