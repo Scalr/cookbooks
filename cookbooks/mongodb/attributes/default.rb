4bb334212f7a3898e8b3983c8728cf67ed0909b0
Eugeny Kurkovich <stunko@scalr.com>
2014-05-07 10:38:56 +0400

    SCALARIZR-1409 *Fix recpe name, *fix tmp dir name

--- cookbooks/mongodb/attributes/default.rb
+++ cookbooks/mongodb/attributes/default.rb
@@ -1,5 +1,5 @@
 # MongoDB repo settings
 
-default[:repository][:dir] = "/tmp/chef-solo/mongodb-src"
+default[:repository][:dir] = "/var/tmp/mongodb-src"
 default[:repository][:name] = "git://github.com/mongodb/mongo.git"
 default[:repository][:revision] = "r2.6.0"
