39038d9fa95056a580db7c4c8c8029234ebacd7e
Roman Bezruchko <r.bezruchko@scalr.com>
2014-08-14 18:11:12 +0400

    SCALARIZR-1457 added rabbitmq versioning

---
+++ cookbooks/rabbitmq/attributes/default.rb
@@ -0,0 +1,16 @@
+default["rabbitmq"]["version"] = "3.3"
+default["rabbitmq"]["packages"] = {"3.3" => 
+                                   {"debian" => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server_3.3.5-1_all.deb",
+                                    "rhel"   => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server-3.3.5-1.noarch.rpm"}}
+
+default["rabbitmq"]["cache_dir"] = "/tmp"
+
+default["rabbitmq"]["package_url"] = node["rabbitmq"]["packages"].fetch(node["rabbitmq"]["version"]).fetch(node["platform_family"])
+default["rabbitmq"]["package_path"] = File.join(node["rabbitmq"]["cache_dir"], node["rabbitmq"]["package_url"].split('/')[-1])
+default["rabbitmq"]["package_provider"] = 
+    case node["platform_family"]
+    when "debian"
+	    Chef::Provider::Package::Dpkg
+    when "rhel"
+        Chef::Provider::Package::Rpm
+    end
