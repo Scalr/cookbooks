0c4d82e71d2147ad053dd2a322931a23a5ea80eb
Roman Bezruchko <r.bezruchko@scalr.com>
2014-08-28 13:21:32 +0400

    SCALARIZR-1457 use platform_family

--- cookbooks/mysql/attributes/default.rb
+++ cookbooks/mysql/attributes/default.rb
@@ -4,8 +4,8 @@ if node['mysql']['version'] == '5.6' && node['platform'] == 'ubuntu' &&
    node['platform_version'] == '14.04'
     default['mysql']['package_client_name'] = 'mysql-client-5.6'
     default['mysql']['package_server_name'] = 'mysql-server-5.6'
-elsif platform?("centos", "redhat", "oracle")
+elsif platform_family?("rhel")
     default['mysql']['package_client_name'] = 'mysql'
-elsif platform?("ubuntu", "debian")
+elsif platform_family?("debian")
     default['mysql']['package_client_name'] = 'mysql-client'
 end
