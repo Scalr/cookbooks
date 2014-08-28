fd68268d731b039e618a5fe741a8722b7a676b3f
Roman Bezruchko <r.bezruchko@scalr.com>
2014-08-28 13:04:27 +0400

    SCALARIZR-1457 use platform_family

--- cookbooks/postgresql/attributes/default.rb
+++ cookbooks/postgresql/attributes/default.rb
@@ -1,13 +1,13 @@
 default['postgresql']['version'] = '9.3'
 
-case node['platform']
-when "redhat", "centos"
+case node['platform_family']
+when "rhel"
     default['postgresql']['dir'] = "/var/lib/pgsql/#{node['postgresql']['version']}/data"
     default['postgresql']['service_name'] = "postgresql-#{node['postgresql']['version']}"
     default['postgresql']['packages'] = ["postgresql#{node['postgresql']['version'].split('.').join}",
                                                    "postgresql#{node['postgresql']['version'].split('.').join}-server",
                                                    "postgresql#{node['postgresql']['version'].split('.').join}-devel"]
-when "debian", "ubuntu"
+when "debian"
      default['postgresql']['dir'] = "/etc/postgresql/#{node['postgresql']['version']}/main"
      default['postgresql']['service_name'] = "postgresql"
      default['postgresql']['packages'] = ["postgresql-#{node['postgresql']['version']}"] 
