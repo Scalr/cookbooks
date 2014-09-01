b847082773edce884d7ba96541c5eb95f265090c
Roman Bezruchko <r.bezruchko@scalr.com>
2014-09-01 13:25:21 +0400

    SCALARIZR-1457 fixed mariadb on amazon

--- cookbooks/mariadb/recipes/default.rb
+++ cookbooks/mariadb/recipes/default.rb
@@ -44,10 +44,9 @@ when 'rhel'
     else
         include_recipe 'yum'
 
-        arch = node[:kernel][:machine] =~ /x86_64/ ? "amd64" : "x86"
         yum_repository 'mariadb' do
             description 'Mariadb repo'
-            baseurl     "http://yum.mariadb.org/#{node[:mariadb][:version]}/#{node[:platform]}#{node[:platform_version].to_i}-#{arch}"
+            baseurl     node["mariadb"]["yum_repo_url"]
             gpgkey      'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB'
         end
 
