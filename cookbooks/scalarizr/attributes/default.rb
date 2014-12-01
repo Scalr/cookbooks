3b546fedbec339d8a39bb12bd33f840f964f0523
Roman Bezruchko <r.bezruchko@scalr.com>
2014-12-01 14:40:47 +0300

    SCALARIZR-1669 handle emtpy branch string

--- cookbooks/scalarizr/attributes/default.rb
+++ cookbooks/scalarizr/attributes/default.rb
@@ -1,3 +1,5 @@
 default[:scalarizr][:behaviour] = [ "base" ]
 default[:scalarizr][:platform]  = "ec2"
 default[:scalarizr][:branch] = "latest"
+# Empty branch string also means we want latest
+node.override["scalarizr"]["branch"] = "latest" if node["scalarizr"]["branch"].strip.empty?
