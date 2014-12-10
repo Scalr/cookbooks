a3f0274c69aab63ea4900d4511be49af458f6e2e
Roman Bezruchko <r.bezruchko@scalr.com>
2014-12-10 19:57:20 +0300

    style

--- cookbooks/haproxy/recipes/default.rb
+++ cookbooks/haproxy/recipes/default.rb
@@ -1,8 +1,8 @@
 if node['platform'] == 'debian' and node['lsb']['codename'] == 'wheezy'
-	file "/etc/apt/sources.list.d/wheezy-backports.list" do
-		content "deb http://ftp.debian.org/debian/ wheezy-backports main"
-	end
-	execute "apt-get update"
+    file "/etc/apt/sources.list.d/wheezy-backports.list" do
+        content "deb http://ftp.debian.org/debian/ wheezy-backports main"
+    end
+    execute "apt-get update"
 end
 
 package "haproxy"
