22d7c71865ba17bce318d93d60db58093b90eda4
Roman Bezruchko <r.bezruchko@scalr.com>
2014-12-11 17:06:15 +0300

    deleted unneeded cookbooks

--- cookbooks/django/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Scalr Inc."
-maintainer_email "packages@scalr.net"
-license          "All rights reserved"
-description      "Installs/Configures Django framework to work with apache and mod_wsgi"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
-depends          "apache2"
-name             "django"
--- cookbooks/rails/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Scalr Inc."
-maintainer_email "packages@scalr.net"
-license          "All rights reserved"
-description      "Installs/Configures Ruby on Rails to work with apache and passenger"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
-depends          "apache2"
-name             "rails"
