7e4ca31dd40fa6221c60fa0033852f2974bceeec
Roman Bezruchko <r.bezruchko@scalr.com>
2014-12-16 18:37:24 +0300

    updated metadata

--- cookbooks/scalarizr_proxy/metadata.rb
+++ cookbooks/scalarizr_proxy/metadata.rb
@@ -5,3 +5,4 @@ description      "Installs and configures scalarizr proxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 name             "scalarizr_proxy"
+supports         "ubuntu"
