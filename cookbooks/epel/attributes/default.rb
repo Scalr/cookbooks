065066ed42d95f0840b2a90a53ae9bf4bb31e234
Marat Komarov <mac.komarov@gmail.com>
2014-12-29 17:42:42 +0200

    corrected EPEL version

--- cookbooks/epel/attributes/default.rb
+++ cookbooks/epel/attributes/default.rb
@@ -1,7 +1,7 @@
 default["epel"]["version"] =
     if (node["platform_version"].to_f >= 6.0 and node["platform_version"].to_f < 7) or node["platform"] == "amazon"
         "6"
-    elsif node["platform"] == 'centos' and node["platform_version"].to_i == 7
+    elsif node["platform_version"].to_i == 7
         "7"
     else
         "5"
