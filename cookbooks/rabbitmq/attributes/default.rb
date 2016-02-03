c6430ea2711b01ad555b8bd0d2b5bfa9eea8419e
Eugeny Kurkovich <stunko@scalr.com>
2016-02-03 20:09:10 +0300

    SCALARIZR-2130 Added rabbitmq 3.6

--- cookbooks/rabbitmq/attributes/default.rb
+++ cookbooks/rabbitmq/attributes/default.rb
@@ -1,19 +1,45 @@
-default["rabbitmq"]["version"] = "3.5"
-default["rabbitmq"]["packages"] = {"3.3" =>
-                                   {"debian" => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server_3.3.5-1_all.deb",
-                                    "rhel"   => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server-3.3.5-1.noarch.rpm"},
-                                    "3.4" =>
-                                   {"debian" => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server_3.4.2-1_all.deb",
-                                    "rhel"   => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server-3.4.2-1.noarch.rpm"},
-                                    "3.5" =>
-                                   {"debian" => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server_3.5.3-1_all.deb",
-                                    "rhel"   => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server-3.5.3-1.noarch.rpm"}}
+default["rabbitmq"]["version"] = "3.6"
+default["rabbitmq"]["packages"] = {
+    "3.3" => {
+        "debian" => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server_3.3.5-1_all.deb",
+        "rhel"   => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server-3.3.5-1.noarch.rpm"},
+    "3.4" => {
+        "debian" => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server_3.4.2-1_all.deb",
+        "rhel"   => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server-3.4.2-1.noarch.rpm"},
+    "3.5" => {
+        "debian" => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server_3.5.3-1_all.deb",
+        "rhel"   => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server-3.5.3-1.noarch.rpm"},
+    "3.6" => {
+        "debian" => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server_3.6.0-1_all.deb",
+        "rhel"   => "https://s3.amazonaws.com/scalr-labs/packages/rabbitmq-server-3.6.0-1.noarch.rpm"}}
 
-default["rabbitmq"]["cache_dir"] = "/tmp"
+default["esl_erlang"]["packages"] = {
+    "debian" => "https://s3.amazonaws.com/scalr-labs/packages/esl-erlang_18.2-1~debian~wheezy_amd64.deb",
+    "rhel" => {
+        "6" => "https://s3.amazonaws.com/scalr-labs/packages/erlang-18.2-1.el6.x86_64.rpm",
+        "7" => "https://s3.amazonaws.com/scalr-labs/packages/erlang-18.2-1.el7.centos.x86_64.rpm"}}
+
+default["packages"]["cache_dir"] = "/tmp"
 
 default["rabbitmq"]["package_url"] = node["rabbitmq"]["packages"].fetch(node["rabbitmq"]["version"]).fetch(node["platform_family"])
-default["rabbitmq"]["package_path"] = File.join(node["rabbitmq"]["cache_dir"], node["rabbitmq"]["package_url"].split('/')[-1])
-default["rabbitmq"]["package_provider"] = 
+default["rabbitmq"]["package_path"] = File.join(node["packages"]["cache_dir"], node["rabbitmq"]["package_url"].split('/')[-1])
+
+esl_erlang_packages = node["esl_erlang"]["packages"].fetch(node["platform_family"])
+default["esl_erlang"]["package_url"] =
+    if node["platform_family"] == "debian"
+        esl_erlang_packages
+    elsif node["platform"] == "amazon"
+        esl_erlang_packages["6"]
+    else
+        esl_erlang_packages.fetch(node["platform_version"].to_i)
+    end
+default["esl_erlang"]["package_path"] = File.join(node["packages"]["cache_dir"], node["esl_erlang"]["package_url"].split('/')[-1])
+default["erlang"]["packages"] = {
+    "debian" => "erlang-nox",
+    "rhel" => "erlang"}
+default["erlang"]["package"] = node["erlang"]["packages"].fetch(node["platform_family"])
+
+default["packages"]["package_provider"] =
     case node["platform_family"]
     when "debian"
         Chef::Provider::Package::Dpkg
--- cookbooks/rabbitmq/recipes/default.rb
+++ cookbooks/rabbitmq/recipes/default.rb
@@ -1,13 +1,43 @@
+if node["rabbitmq"]["version"] == "3.6"
+    remote_file node["esl_erlang"]["package_path"] do
+        source node["esl_erlang"]["package_url"]
+    end
+    package "custom_erlang" do
+        source node["esl_erlang"]["package_path"]
+        provider node["packages"]["package_provider"]
+        case node["platform_family"]
+        when "debian"
+            ignore_failure true
+        end
+    end
+else
+    package "custom_erlang" do
+        package_name node["erlang"]["package"]
+    end
+end
+
 case node["platform_family"]
 when "debian"
+    execute "apt-get update"
     package "logrotate"
-    package "erlang-nox"
 
 when "rhel"
     raise if node["platform_version"].to_f < 6
 
     include_recipe "epel"
-    package "erlang"
+end
+
+package "custom_erlang" do
+    action :install
+end
+
+execute "install-dependencies" do
+    case node["platform_family"]
+    when "debian"
+        command "apt-get -yf install"
+        action :run
+        only_if { File.exists?(node["esl_erlang"]["package_path"])}
+    end
 end
 
 remote_file node["rabbitmq"]["package_path"] do
@@ -16,10 +46,9 @@ end
 
 package "rabbitmq" do
     source node["rabbitmq"]["package_path"]
-    provider node["rabbitmq"]["package_provider"]
+    provider node["packages"]["package_provider"]
     action :install
 end
-
 
 service "rabbitmq-server" do
     action [:disable, :stop]
